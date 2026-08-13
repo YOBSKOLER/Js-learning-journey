ba3d9ebade68a10ff535acfd2ecdf3a3915c595f
YOBS KOLER <yobskoler9@gmail.com>
2026-08-13 08:21:28 +0100

    fondamentals

---
+++ fondamentals/script.js
@@ -0,0 +1,25 @@
+/*
+
+Ex 1
+personnal presentation
+
+write a script that will present yourself in the console. You can use console.log() to display your name, age, and a short description about yourself.          
+
+*/
+
+let name = "yobs koler";
+const age = 22;
+const city = "Douala";
+const Language = "JavaScript";
+const formation = "I'am doing a specialisaion in sofware engeneering ";
+
+console.log(
+  ` Goodmorning my name is ${name}, i have ${age} years old and i'm living at ${city} . actually ${formation}, m favorite programming language is ${Language} `,
+);
+
+if (city) {
+  console.log("i am the best");
+}
+
+// the diff btw le and const is that (let) can be declare and initialise later  while const is declare and initialise
+// the variable daclare with (let) can be change through the code or the process while with cons is not possible   
